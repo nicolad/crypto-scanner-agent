d186415ed2033c33b7baa4e0e0093481c7bbedbc
Vadim Nicolai <nicolai.vadim@gmail.com>
2025-06-07 20:02:36 +0300

    docs: reference library

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub mod util {
     }
 }
 
-/// The version of the `crypto-scanner-agent` crate. This is populated at
+/// The version of the `crypto-scanner-agent` library. This is populated at
 /// compile time using the `CARGO_PKG_VERSION` environment variable.
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
